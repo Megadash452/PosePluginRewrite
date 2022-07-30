5cfa6e490c7061f50d70e973747b6dc4af2823d3
Megadash452 <marticraft680@gmail.com>
2022-07-29 23:12:37 -0400

    Remove unused main

--- src/main/kotlin/PosePluginRewrite.kt
+++ src/main/kotlin/PosePluginRewrite.kt
@@ -3,14 +3,6 @@ package me.megadash452.posepluginrewrite
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
-// fun main(args: Array<String>) {
-//     println("Hello World!")
-//
-//     // Try adding program arguments via Run/Debug configuration.
-//     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-//     println("Program arguments: ${args.joinToString()}")
-// }
-
 class PosePluginRewrite : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
